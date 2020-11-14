f74bb0e159e85e5ada60088132556d4ae0260230
PhanNgocHoang <hoangpn.dev@gmail.com>
2020-11-14 10:41:37 +0700

    test

--- platforms/browser/www/js/database.js
+++ platforms/browser/www/js/database.js
@@ -73,7 +73,7 @@ async function AddRes(collectionName, data) {
         navigator.notification.beep(1);
         navigator.vibrate(100)
         alert("You Rated Successfully")
-        window.location = "index.html"
+        $(location).attr('href', "#index")
         $('listrest').empty()
         home()
     }
